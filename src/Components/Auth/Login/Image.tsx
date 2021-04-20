221031688d8589798f57effd3e5ba18110e047df
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-21 02:25:53 +0900

    Insert the background image of the login page.

---
+++ src/Components/Auth/Login/Image.tsx
@@ -0,0 +1,13 @@
+import styled from "styled-components";
+
+const Image = styled.img`
+  width: 100%;
+  height: 100vh;
+  z-index: -1000;
+  position: absolute;
+  top: 0;
+  left: 0;
+  filter: brightness(0.4);
+`;
+
+export default Image;
