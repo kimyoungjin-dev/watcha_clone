3097a9bb6f9bb40acc658588ca6abade5f5557ee
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-21 08:58:17 +0900

    Modify CSS

--- src/Components/Auth/Input.tsx
+++ src/Components/Auth/Input.tsx
@@ -4,14 +4,8 @@ const Input = styled.input`
   width: 100%;
   background-color: white;
   padding: 15px 0px;
-  &:first-child {
-    border-top-right-radius: 5px;
-    border-top-left-radius: 5px;
-  }
-  &:nth-child(2) {
-    border-bottom-right-radius: 5px;
-    border-bottom-left-radius: 5px;
-  }
+  padding-left: 5px;
+
   &::placeholder {
     color: ${(props) => props.theme.silverColor};
     padding-left: 5px;
