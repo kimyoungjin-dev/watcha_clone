734ce0c1e5fba3b3659eb4c2dfd82a60d540db78
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-21 02:25:15 +0900

    Apply GlobalStyles.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -1,15 +1,15 @@
 import { useReactiveVar } from "@apollo/client";
-import React from "react";
 import { darkVar } from "./apollo";
 import Router from "./Router";
 import { ThemeProvider } from "styled-components";
-import { lightTheme } from "../Styles/theme";
+import { lightTheme, GlobalStyles } from "../Styles/styles";
 
 export default function App() {
   const darkMode = useReactiveVar(darkVar);
 
   return (
     <ThemeProvider theme={lightTheme}>
+      <GlobalStyles />
       <Router />
     </ThemeProvider>
   );
