72a58bd3110bdd3c9d42373b1c6e05c03219f346
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-04-22 15:25:15 +0900

    Data component to be used in CheckList

---
+++ src/Components/Auth/SignUp/Data.tsx
@@ -0,0 +1,21 @@
+export const data = [
+  { title: "전체 약관에 동의합니다", id: 1, check: false },
+
+  { title: "만 14세 이상입니다", id: 2, check: false },
+  {
+    title: "왓챠피디아 서비스 이용약관에 동의합니다 (필수)",
+    id: 3,
+    check: false,
+  },
+  { title: "왓챠 서비스 이용약관에 동의합니다 (필수)", id: 4, check: false },
+  {
+    title: "개인정보 수집 및 이용에 대한 안내에 동의합니다 (필수)",
+    id: 5,
+    check: false,
+  },
+  {
+    title: "신작 알림 이벤트 정보 수신에 동의합니다 (선택)",
+    id: 6,
+    check: false,
+  },
+];
