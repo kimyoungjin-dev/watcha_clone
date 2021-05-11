a6cb8e52e7512852afab05c7a01c0c861067f3b2
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-11 18:59:16 +0900

    Dark mode in the theme value in ThemeProvider is set to dark mode, and if not in dark mode, it is set to white mode.

--- src/Components/App.tsx
+++ src/Components/App.tsx
@@ -2,13 +2,13 @@ import { useReactiveVar } from "@apollo/client";
 import { darkVar } from "./apollo";
 import Router from "./Router";
 import { ThemeProvider } from "styled-components";
-import { lightTheme, GlobalStyles } from "../Styles/styles";
+import { lightTheme, GlobalStyles, darkTheme } from "../Styles/styles";
 
 export default function App() {
   const darkMode = useReactiveVar(darkVar);
 
   return (
-    <ThemeProvider theme={lightTheme}>
+    <ThemeProvider theme={darkMode ? darkTheme : lightTheme}>
       <GlobalStyles />
       <Router />
     </ThemeProvider>
