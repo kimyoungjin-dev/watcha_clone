4aad85cc2f63a253cd5e98d69d7296561908cf6d
kim young jin <75160032+kimyoungjin-dev@users.noreply.github.com>
2021-05-11 18:55:20 +0900

    In order to make darkVar, if it is dark mode?=> Put token.

--- src/Components/apollo.ts
+++ src/Components/apollo.ts
@@ -1,4 +1,16 @@
 import { makeVar } from "@apollo/client";
 
+const DARKITEM = "DARKITEM";
+
 export const isLoggedInVar = makeVar(false);
-export const darkVar = makeVar(false);
+export const darkVar = makeVar(Boolean(localStorage.getItem(DARKITEM)));
+
+export const darkChange = () => {
+  localStorage.setItem(DARKITEM, "enabled");
+  darkVar(true);
+};
+
+export const lightChange = () => {
+  localStorage.removeItem(DARKITEM);
+  darkVar(false);
+};
